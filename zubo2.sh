c465a62cf40126289b7e7c2336013b4aaf1b52ac
yktcfy <zjyktcfy@qq.com>
2025-05-14 12:49:17 +0800

    Update zubo2.sh

--- zubo2.sh
+++ zubo2.sh
@@ -1,121 +1,148 @@
 #pwd
 if [ $# -eq 0 ]; then
-  echo "开始测试······"
-  echo "在5秒内输入1~4可选择城市"
-  echo "1.浙江电信"
-  echo "2.江苏电信"
-  echo "3.天津联通"
-  echo "4.湖北电信"
-  echo "5.河南电信"
-  read -t 5 -p "超时未输入,将按默认设置测试" city_choice
+  echo "请选择城市："
+  echo "1. 天津（Tianjin_liantong）"
+  echo "2. 浙江（Zhejiang_dianxin）"
+  echo "3. 北京（Beijing_liantong）"
+  echo "4. 上海（Shanghai_dianxin）"
+  echo "5. 广东（Guangdong_dianxin）"
+  echo "6. 江苏（Jiangsu_dianxin）"
+  echo "7. 安徽（Anhui_dianxin）"
+  echo "8. 湖北（Hubei_dianxin）"
+  echo "9. 重庆（Chongqing_liantong）"
+  echo "10. 湖南（Hunan_dianxin）"
+  echo "11. 河南（Henan_liantong）"
+  echo "12. 河北（Hebei_liantong）"
+  echo "13. KR（KR_SK）"
+  echo "0. 全部"
+  read -t 10 -p "输入选择或在10秒内无输入将默认选择全部: " city_choice
 
   if [ -z "$city_choice" ]; then
-      echo "未检测到输入,默认测试全部"
+      echo "未检测到输入，自动选择全部选项..."
       city_choice=0
   fi
 
 else
   city_choice=$1
 fi
-# 设置城市和相应的stream
+
+# 根据用户选择设置城市和相应的stream
 case $city_choice in
     1)
-        city="浙江电信"
-        stream="udp/233.50.201.100:5140"
-        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249Iua1meaxnyIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page=1&page_size=20"
+        city="Tianjin_liantong"
+        stream="rtp/225.1.1.149:5002"
+	channel_key="天津"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Tianjin" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJUaWFuamluIiAmJiBwcm90b2NvbD0iaHR0cCI%3D&page_size=20"$url_fofa
+        url_fofa="https://foo.yangfeiyue.dpdns.org/Tianjin_liantong.txt"$url_fofa
         ;;
     2)
-        city="江苏电信"
-        stream="udp/239.49.8.19:9614"
-        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249Iuaxn%2BiLjyIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page=1&page_size=10"
-        ;;
-    5)
-        city="河北联通"
-        stream="rtp/239.253.92.154:6011"
+        city="Zhejiang_dianxin"
+        stream="rtp/233.50.201.136:5140"
+        channel_key="浙江"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Zhejiang" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJaaGVqaWFuZyIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page_size=20"$url_fofa
+        url_fofa="https://foo.yangfeiyue.dpdns.org/Zhejiang_dianxin.txt"$url_fofa
         ;;
     3)
-        city="湖北电信"
-        stream="rtp/239.69.1.40:9880"
-        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249Iua5luWMlyIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page=1&page_size=20"
+        city="Beijing_liantong"
+        stream="rtp/239.3.1.250:8001"
+        channel_key="北京"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Beijing" && org="China Unicom Beijing Province Network" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJCZWlqaW5nIiAmJiBvcmc9IkNoaW5hIFVuaWNvbSBCZWlqaW5nIFByb3ZpbmNlIE5ldHdvcmsiICYmIHByb3RvY29sPSJodHRwIg%3D
+        url_fofa="https://foo.yangfeiyue.dpdns.org/Beijing_liantong.txt"$url_fofa
         ;;
     4)
-        city="河南电信"
-        stream="rtp/239.16.20.21:10210"
+        city="Shanghai_dianxin"
+        stream="rtp/239.45.3.237:5140"
+        channel_key="上海"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Shanghai" && org="China Telecom Group" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJTaGFuZ2hhaSIgJiYgb3JnPSJDaGluYSBUZWxlY29tIEdyb3VwIiAmJiBwcm90b2NvbD0iaHR0cCI%3D&page_size=20"$url_fofa
+        url_fofa="https://foo.yangfeiyue.dpdns.org/Shanghai_dianxin.txt"$url_fofa
+        ;;
+    5)
+        city="Guangdong_dianxin"
+        stream="rtp/239.77.0.217:5146"
+        channel_key="广东"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Guangdong" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJHdWFuZ2RvbmciICYmIG9yZz0iQ2hpbmFuZXQiICYmIHByb3RvY29sPSJodHRwIg%3D%3D&page_size=20"$url_fofa
+        url_fofa="https://foo.yangfeiyue.dpdns.org/Guangdong_dianxin.txt"$url_fofa
         ;;
     6)
-        city="广东电信"
-        stream="udp/239.77.1.152:5146"
+        city="Jiangsu_dianxin"
+        stream="rtp/239.49.8.118:8000"
+        channel_key="江苏"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Jiangsu" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJKaWFuZ3N1IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBwcm90b2NvbD0iaHR0cCI%3D&page_size=20"$url_fofa
+        url_fofa="https://faa.yangfeiyue.dpdns.org/Jiangsu_dianxin.txt"$url_fofa
         ;;
     7)
-        city="北京联通"
-        stream="rtp/239.3.1.241:8000"
-	;;
+        city="Anhui_dianxin"
+        stream="rtp/238.1.79.42:4448"
+        channel_key="安徽"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Anhui" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJBbmh1aSIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page_size=20"$url_fofa
+        url_fofa="https://faa.yangfeiyue.dpdns.org/Anhui_dianxin.txt"$url_fofa
+        ;;
     8)
-        city="湖南电信"
-        stream="udp/239.76.246.151:1234"
-	;;
+        city="Hubei_dianxin"
+        stream="rtp/239.69.1.204:10866"
+        channel_key="湖北"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Hubei" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJIdWJlaSIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page_size=20"$url_fofa
+        url_fofa="https://faa.yangfeiyue.dpdns.org/Hubei_dianxin.txt"$url_fofa
+        ;;
     9)
-        city="广东联通"
-        stream="udp/239.0.1.1:5001"
-	;;
+        city="Chongqing_liantong"
+        stream="rtp/225.0.4.192:7980"
+        channel_key="重庆"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Chongqing" && org="CHINA UNICOM China169 Backbone" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJDaG9uZ3FpbmciICYmIG9yZz0iQ0hJTkEgVU5JQ09NIENoaW5hMTY5IEJhY2tib25lIiAmJiBwcm90b2NvbD0iaHR0cCI%3D&page_size=20"$url_fofa
+        url_fofa="https://faa.yangfeiyue.dpdns.org/Chongqing_liantong.txt"$url_fofa
+        ;;
     10)
-        city="四川电信"
-        stream="udp/239.93.0.169:5140"
+        city="Hunan_dianxin"
+        stream="rtp/239.76.245.119:1234"
+        channel_key="湖南"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Hunan" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJIdW5hbiIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page_size=20"$url_fofa
+        url_fofa="https://faa.yangfeiyue.dpdns.org/Hunan_dianxin.txt"$url_fofa
         ;;
     11)
-        city="山东电信"
-        stream="udp/239.21.1.87:5002"
+        city="Henan_liantong"
+        stream="rtp/225.1.4.55:1084"
+        channel_key="河南"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Henan" && org="CHINA UNICOM China169 Backbone" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJIZW5hbiIgJiYgb3JnPSJDSElOQSBVTklDT00gQ2hpbmExNjkgQmFja2JvbmUiICYmIHByb3RvY29sPSJodHRwIg%3D%3D&page_size=20"$url_fofa
+        url_fofa="https://fuu.yangfeiyue.dpdns.org/Henan_liantong.txt"$url_fofa
         ;;
     12)
-        city="陕西电信"
-        stream="rtp/239.111.205.35:5140"
+        city="Hebei_liantong"
+        stream="rtp/239.253.92.173:6003"
+        channel_key="河北"
+#        url_fofa=$(echo  '"udpxy" && country="CN" && region="Hebei" && org="CHINA UNICOM China169 Backbone" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJIZWJlaSIgJiYgb3JnPSJDSElOQSBVTklDT00gQ2hpbmExNjkgQmFja2JvbmUiICYmIHByb3RvY29sPSJodHRwIg%3D%3D&page_size=20"$url_fofa
+        url_fofa="https://fuu.yangfeiyue.dpdns.org/Hebei_liantong.txt"$url_fofa
         ;;
     13)
-        city="广西电信"
-        stream="udp/239.81.0.107:4056"
-        ;;
-    14)
-        city="贵州电信"
-        stream="rtp/238.255.2.1:5999"
-        ;;
-    15)
-        city="山西联通"
-        stream="rtp/226.0.2.152:9128"
-        ;;
-    16)
-        city="上海电信"
-        stream="udp/239.45.3.146:5140"
-        ;;
-    17)
-        city="福建电信"
-        stream="rtp/239.61.2.132:8708"
-        ;;
-    18)
-        city="江西电信"
-        stream="udp/239.252.220.63:5140"
-        ;;
-    19)
-        city="安徽电信"
-        stream="rtp/238.1.79.27:4328"
-        ;;
-    20)
-        city="天津联通"
-        stream="udp/225.1.1.111:5002"
-        ;;
-    21)
-        city="宁夏电信"
-        stream="rtp/239.121.4.94:8538"
-        ;;
-    22)
-        city="重庆电信"
-        stream="rtp/235.254.196.249:1268"
+        city="KR_SK"
+        stream="udp/239.192.67.228:49220"
+        channel_key="KR"
+#        url_fofa=$(echo  '"udpxy" && country="KR" && org="SK Broadband Co Ltd" && protocol="http"' | base64 |tr -d '\n')
+#        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJLUiIgJiYgb3JnPSJTSyBCcm9hZGJhbmQgQ28gTHRkIiAmJiBwcm90b2NvbD0iaHR0cCI%3D&page_size=20"$url_fofa
+        url_fofa="https://lii.yangfeiyue.dpdns.org/KR_SK.txt"$url_fofa
         ;;
     0)
-        # 逐个处理{ }内每个选项
-        for option in {1..22}; do
-          bash "$0" $option  # 假定fofa.sh是当前脚本的文件名，$option将递归调用
+        # 如果选择是“全部选项”，则逐个处理每个选项
+        for option in {1..20}; do
+          bash  "$0" $option  # 假定fofa.sh是当前脚本的文件名，$option将递归调用
         done
         exit 0
+        ;;
+
+    *)
+        echo "错误：无效的选择。"
+        exit 1
         ;;
 esac
 
